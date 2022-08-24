5076dacb019af0f1336c4b6915577e870f6e9a29
NileshKoder <nileshsulakhe1@gmail.com>
2022-08-24 20:03:51 +0530

    fix: add role in fillable

--- app/User.php
+++ app/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable implements UserConstants
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'role'
     ];
 
     /**

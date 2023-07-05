a30b48f818ba99c0111481b24fac41db7069c752
NileshKoder <nileshsulakhe1@gmail.com>
2023-07-06 00:24:31 +0530

    change: add routes

--- app/Features/Process/Routes/api.php
+++ app/Features/Process/Routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Features\Process\ReachTruck\Http\Controllers\Api\ReachTruckApiController;
 
 Route::post('pallets/create', 'App\Features\Process\PalletManagement\Http\v1\Controllers\Api\PalletApiController@create');
 Route::post('pallets/get-pallet-details', 'App\Features\Process\PalletManagement\Http\v1\Controllers\Api\PalletApiController@getPalletDetails');
 Route::post('pallets/store', 'App\Features\Process\PalletManagement\Http\v1\Controllers\Api\PalletApiController@store');
 Route::put('pallets/update/{pallet}', 'App\Features\Process\PalletManagement\Http\v1\Controllers\Api\PalletApiController@update');
+
+Route::post('reach-truck/home', [ReachTruckApiController::class, 'home']);
+Route::post('reach-truck/create', [ReachTruckApiController::class, 'getCreateData']);
+Route::post('reach-truck/store', [ReachTruckApiController::class, 'store']);
+Route::post('reach-truck/get-pallet-for-reach-truck', [ReachTruckApiController::class, 'getPalletForReachTruck']);

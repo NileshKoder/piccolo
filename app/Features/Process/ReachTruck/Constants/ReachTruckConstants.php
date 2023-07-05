d60138e1e7459da5f9467cc8208002a1f4e92bbe
NileshKoder <nileshsulakhe1@gmail.com>
2023-07-05 23:58:56 +0530

    change: add rules

--- app/Features/Process/ReachTruck/Constants/ReachTruckConstants.php
+++ app/Features/Process/ReachTruck/Constants/ReachTruckConstants.php
@@ -4,5 +4,12 @@ namespace App\Features\Process\ReachTruck\Constants;
 
 interface ReachTruckConstants
 {
-    //
+    public const CREATE_RULES = [
+        "transfered_by" => 'required|exists:users,id',
+        "reach_truck_id" => 'required|exists:reach_trucks,id',
+        "from_locationable_type" => 'required',
+        "from_locationable_id" => 'required',
+        "to_locationable_type" => 'required',
+        "to_locationable_id" => 'required',
+    ];
 }

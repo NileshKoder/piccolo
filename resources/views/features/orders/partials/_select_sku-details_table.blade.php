9bd18d1fa6c9a4bd0047814df3c8b1fe7e629100
NileshKoder <nileshsulakhe1@gmail.com>
2023-07-27 23:27:44 +0530

    change: add details for coding

--- resources/views/features/orders/partials/_select_sku-details_table.blade.php
+++ resources/views/features/orders/partials/_select_sku-details_table.blade.php
@@ -8,11 +8,53 @@
                     <th>Required Weight</th>
                     <th>Date</th>
                     <th>Drop Location</th>
+                    <th>Status</th>
                     <th>Action</th>
                 </tr>
             </thead>
             <tbody id="tbody">
-
+                @if(!empty($order))
+                @foreach ($order->ordeItems as $key => $ordeItem)
+                <tr>
+                    <input type="hidden" class="order_item_detail_id" name="order_item_details[{{ $key }}][order_item_id]" value="{{ $ordeItem->id }}">
+                    <td>
+                        <input type="hidden" class="selected_sku_id" name="order_item_details[{{ $key }}][sku_code_id]" value="{{ $ordeItem->sku_code_id }}">
+                        <input type="text" class="form-control" value="{{ $ordeItem->skuCode->name }}" disabled>
+                    </td>
+                    <td>
+                        <input type="hidden" class="selected_variant_id" name="order_item_details[{{ $key }}][variant_id]" value="{{ $ordeItem->variant_id }}">
+                        <input type="text" class="form-control" value="{{ $ordeItem->variant->name }}" disabled>
+                    </td>
+                    <td>
+                        <input type="hidden" name="order_item_details[{{ $key }}][required_weight]" value="{{ $ordeItem->required_weight }}">
+                        <input type="text" class="form-control" value="{{ $ordeItem->required_weight }}" disabled>
+                    </td>
+                    <td>
+                        <input type="hidden" name="order_item_details[{{ $key }}][pick_up_date]" value="{{ date('d/m/Y', strtotime($ordeItem->pick_up_date)) }}">
+                        <input type="text" class="form-control" value="{{ date('d/m/Y', strtotime($ordeItem->pick_up_date)) }}" disabled>
+                    </td>
+                    <td>
+                        <input type="hidden" name="order_item_details[{{ $key }}][location_id]" class="weight" value="{{ $ordeItem->location_id }}">
+                        <input type="text" class="form-control" value="{{ $ordeItem->location->abbr }}" disabled>
+                    </td>
+                    <td>
+                        <select name="order_item_details[{{ $key }}][state]" class="form-control state">
+                            <option value="">Select State</option>
+                            @foreach ($masterData['orderItemStates'] as $orderItemState)
+                            <option value="{{ $orderItemState }}" @if($ordeItem->state == $orderItemState) selected @endif>{{ $orderItemState }}</option>
+                            @endforeach
+                        </select>
+                    </td>
+                    <td class="text-center">
+                        @if($ordeItem->state == $masterData['orderItemCreate'])
+                        <a href="javascript:void(0);" class="text-danger remove-sku">
+                            <i class="fa fa-trash"></i>
+                        </a>
+                        @endif
+                    </td>
+                </tr>
+                @endforeach
+                @endif
             </tbody>
         </table>
     </div>

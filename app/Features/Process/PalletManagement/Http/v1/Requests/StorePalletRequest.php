19841a707a34992042224b4c6843abbce3936188
NileshKoder <nileshsulakhe1@gmail.com>
2023-07-29 01:54:56 +0530

    change: get rules from model and remove unwanted code

--- app/Features/Process/PalletManagement/Http/v1/Requests/StorePalletRequest.php
+++ app/Features/Process/PalletManagement/Http/v1/Requests/StorePalletRequest.php
@@ -2,8 +2,9 @@
 
 namespace App\Features\Process\PalletManagement\Http\v1\Requests;
 
-use App\Features\Masters\Locations\Domains\Models\Location;
 use Illuminate\Foundation\Http\FormRequest;
+use App\Features\Masters\Locations\Domains\Models\Location;
+use App\Features\Process\PalletManagement\Domains\Models\Pallet;
 
 class StorePalletRequest extends FormRequest
 {
@@ -24,28 +25,17 @@ class StorePalletRequest extends FormRequest
      */
     public function rules()
     {
-        return [
-            "location_id" => 'required|exists:locations,id',
-            "master_pallet_id" => 'required|exists:master_pallets,id',
-            "pallet_details.*.sku_code_id" => 'required|exists:sku_codes,id',
-            "pallet_details.*.variant_id" => 'required|exists:variants,id',
-            "pallet_details.*.weight" => 'required',
-            "pallet_details.*.batch" => 'required',
-        ];
+        return Pallet::getCreateValidationRules();
     }
 
     public function requestData(): array
     {
         $requestData = [];
         $requestData['pallet']['master_pallet_id'] = $this->master_pallet_id;
+        $requestData['pallet']['location_id'] = $this->location_id;
         $requestData['pallet']['created_by'] = auth()->check() ? auth()->user()->id : 1;
         $requestData['pallet']['updated_by'] = auth()->check() ? auth()->user()->id : 1;
-
         $requestData['pallet_details'] = $this->pallet_details;
-
-        $requestData['pallet_location']['locationable_type'] = Location::class;
-        $requestData['pallet_location']['locationable_id'] = $this->location_id;
-        $requestData['pallet_location']['created_by'] = auth()->check() ? auth()->user()->id : 1;
 
         $requestData['is_request_for_warehouse'] = !empty($this->request_for_warehouse) ? true : false;
 

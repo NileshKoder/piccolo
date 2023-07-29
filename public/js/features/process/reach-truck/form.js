8bcdf1d7ec6e58b5e499c75f5bb0cd767d1bbdb3
NileshKoder <nileshsulakhe1@gmail.com>
2023-07-29 20:01:32 +0530

    change: to locationable id change if from location is warehouse

--- public/js/features/process/reach-truck/form.js
+++ public/js/features/process/reach-truck/form.js
@@ -15,7 +15,7 @@ $(document).on('change', '#from_locationable_id', function() {
             $('#reach_truck_id').empty();
             $('#reach_truck_id').append(`<option value="">Select Pallet</option>`);
             $.each(response, function(index, value) {
-                $('#reach_truck_id').append(`<option value="${value.id}">${value.pallet.master_pallet.name}</option>`);
+                $('#reach_truck_id').append(`<option value="${value.id}" data-to_loaction_id="${value.to_locationable_id}">${value.pallet.master_pallet.name}</option>`);
             })
         },
         error: function(data) {
@@ -31,17 +31,23 @@ $(document).on('change', "#reach_truck_id", function() {
     if (selectedPallet.charAt(0) == "C") {
         $('#to_locationable_id').val(1).trigger('change')
     }
+
+    if($('#from_locationable_type').val() == "App\\Features\\Masters\\Warehouses\\Domains\\Models\\Warehouse") {
+        let toLocationId = $('#reach_truck_id option:selected').data('to_loaction_id');
+        $('#to_locationable_id').val(toLocationId).trigger('change')
+    }
 })
 
 
 $(document).on("mouseover", ".to_locationable_id_div", function() {
-    if ($('#to_locationable_id').val() == 1) {
+    if ($('#to_locationable_id').val() == 1 || $('#from_locationable_type').val() == "App\\Features\\Masters\\Warehouses\\Domains\\Models\\Warehouse") {
         $(this).find('select').prop('disabled', true);
     }
 });
 
 $(document).on("mouseleave", ".to_locationable_id_div", function() {
-    if ($('#to_locationable_id').val() == 1) {
+    if ($('#to_locationable_id').val() == 1 || $('#from_locationable_type').val() == "App\\Features\\Masters\\Warehouses\\Domains\\Models\\Warehouse") {
         $(this).find('select').removeAttr('disabled');
     }
 });
+

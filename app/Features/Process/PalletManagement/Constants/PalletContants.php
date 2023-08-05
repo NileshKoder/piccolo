d8346faedab5bff5b86ccf3dcb9503e24659ecc0
NileshKoder <nileshsulakhe1@gmail.com>
2023-08-05 16:15:11 +0530

    change: pallet weight

--- app/Features/Process/PalletManagement/Constants/PalletContants.php
+++ app/Features/Process/PalletManagement/Constants/PalletContants.php
@@ -4,6 +4,6 @@ namespace App\Features\Process\PalletManagement\Constants;
 
 interface PalletContants
 {
-    public const MAX_WEIGHT_FOR_PALLET = 900;
+    public const MAX_WEIGHT_FOR_PALLET = 945;
     public const MAX_WEIGHT_FOR_CONTAINER = 1500;
 }

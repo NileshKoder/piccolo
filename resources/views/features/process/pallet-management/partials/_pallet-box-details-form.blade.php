8838c5ffaa7196f84c0f9b0cfa2f6e88c334ba6e
NileshKoder <nileshsulakhe1@gmail.com>
2023-08-12 02:43:15 +0530

    change: p

---
+++ resources/views/features/process/pallet-management/partials/_pallet-box-details-form.blade.php
@@ -0,0 +1,49 @@
+<div class="row">
+    <div class="col-md-4">
+        <div class="col-md-12">
+            <div class="form-group">
+                <label>Location</label>
+                <select name="location_id" id="location_id" class="form-control select2">
+                    <option value="">Select Locations</option>
+                    @foreach ($data['locations'] as $location)
+                        <option value="{{ $location->id }}" data-abbr="{{ $location->abbr }}" @if(!empty($pallet) && $pallet->masterPallet->last_locationable_type == "App\Features\Masters\Locations\Domains\Models\Location" && $pallet->masterPallet->last_locationable_id == $location->id) selected @endif>{{ $location->name }}</option>
+                    @endforeach
+                </select>
+            </div>
+        </div>
+        <div class="col-md-12">
+            <div class="form-group">
+                <input type="hidden" id="max_weight">
+                <label>Pallet</label>
+                <select name="master_pallet_id" id="master_pallet_id" class="form-control select2">
+                    <option value="">Select Master Pallet</option>
+                    @foreach ($data['masterPallets'] as $masterPallet)
+                        <option value="{{ $masterPallet->id }}" @if(!empty($pallet) && $pallet->master_pallet_id == $masterPallet->id) selected @endif>{{ $masterPallet->name }}</option>
+                    @endforeach
+                </select>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-8">
+         <table class="table table-striped table-bordered">
+            <thead>
+                <tr>
+                    <th>Box Name</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody id="tbody">
+                <input type="hidden" name="pallet_box_details[0][id]" value="">
+                <td>
+                    <input type="text" name="pallet_box_details[0][box_name]" class="form-control" placeholder="Enter Box Names">
+                </td>
+                <td></td>
+            </tbody>
+            <tfoot>
+                <th colspan=2>
+                    <button type="button" class="btn btn-warning" id="add_box_details"><i class="fa fa-plus"></i> Add</button>
+                </th>
+            </tfoot>
+        </table>
+    </div>
+</div>

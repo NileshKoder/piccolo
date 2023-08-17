9b3d4db4792c465da798075193b3259fcf7ebbcd
NileshKoder <nileshsulakhe1@gmail.com>
2023-08-18 03:06:08 +0530

    change: update order state

---
+++ app/Features/OrderManagement/Observers/OrderItemObserver.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Features\OrderManagement\Observers;
+
+use App\Features\OrderManagement\Domains\Models\Order;
+use App\Features\OrderManagement\Domains\Models\OrderItem;
+
+class OrderItemObserver
+{
+    public function updated(OrderItem  $orderItem)
+    {
+        if($orderItem->isDirty('state')) {
+            if ($orderItem->state == OrderItem::TRANSFERED) {
+                $this->processUpdateOrderState($orderItem->order);
+            }
+        }
+    }
+
+    public function processUpdateOrderState(Order $order)
+    {
+        if($order->ordeItems()->count() == $order->ordeItems()->where('state', OrderItem::TRANSFERED)->count())
+        {
+            $order->updateState(Order::COMPLETED);
+        }
+    }
+}

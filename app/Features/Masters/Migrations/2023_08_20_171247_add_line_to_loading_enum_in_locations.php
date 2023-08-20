3b81f4195228ba202fc2a82e337ae92621fc0cac
NileshKoder <nileshsulakhe1@gmail.com>
2023-08-20 23:22:56 +0530

    chore: add enum

---
+++ app/Features/Masters/Migrations/2023_08_20_171247_add_line_to_loading_enum_in_locations.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddLineToLoadingEnumInLocations extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('loading_enum_in_locations', function (Blueprint $table) {
+            DB::statement("ALTER TABLE `locations` CHANGE `type` `type` ENUM('GLASS','CERAMIC','RECYCLE','LINES','CURING','LOADING','LINE_TO_WH','LINE_TO_LOADING') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('loading_enum_in_locations', function (Blueprint $table) {
+            DB::statement("ALTER TABLE `locations` CHANGE `type` `type` ENUM('GLASS','CERAMIC','RECYCLE','LINES','CURING','LOADING','LINE_TO_WH') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;");
+        });
+    }
+}

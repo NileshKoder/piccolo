cc7864bcae0591e7677bb718474916b754498b0a
NileshKoder <nileshsulakhe1@gmail.com>
2023-08-22 02:09:58 +0530

    change: add enum for wh to assembly line

--- app/Features/Masters/Migrations/2023_08_20_171247_add_line_to_loading_enum_in_locations.php
+++ app/Features/Masters/Migrations/2023_08_20_171247_add_line_to_loading_enum_in_locations.php
@@ -14,7 +14,7 @@ class AddLineToLoadingEnumInLocations extends Migration
     public function up()
     {
         Schema::table('loading_enum_in_locations', function (Blueprint $table) {
-            DB::statement("ALTER TABLE `locations` CHANGE `type` `type` ENUM('GLASS','CERAMIC','RECYCLE','LINES','CURING','LOADING','LINE_TO_WH','LINE_TO_LOADING') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;");
+            DB::statement("ALTER TABLE `locations` CHANGE `type` `type` ENUM('GLASS','CERAMIC','RECYCLE','LINES','CURING','LOADING','LINE_TO_WH','LINE_TO_LOADING', 'WH_TO_LINE') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;");
         });
     }
 

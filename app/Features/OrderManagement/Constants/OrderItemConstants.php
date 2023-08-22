a315a5a6a5b87ce432a4cd1feee23f69fbb7035b
NileshKoder <nileshsulakhe1@gmail.com>
2023-08-23 01:25:51 +0530

    change: add new state and fix spell mistake

--- app/Features/OrderManagement/Constants/OrderItemConstants.php
+++ app/Features/OrderManagement/Constants/OrderItemConstants.php
@@ -7,14 +7,16 @@ interface OrderItemConstants
     public const CREATED = "CREATED";
     public const PARTIAL_MAPPED = "PARTIAL_MAPPED";
     public const MAPPED = "MAPPED";
-    public const TRANSFERED = "TRANSFERED";
+    public const PARTIAL_TRANSFERRED = "PARTIAL_TRANSFERRED";
+    public const TRANSFERRED = "TRANSFERRED";
     public const CANCELLED = "CANCELLED";
 
     public const STATES = [
         self::CREATED => self::CREATED,
         self::PARTIAL_MAPPED => self::PARTIAL_MAPPED,
         self::MAPPED => self::MAPPED,
-        self::TRANSFERED => self::TRANSFERED,
+        self::PARTIAL_TRANSFERRED => self::PARTIAL_TRANSFERRED,
+        self::TRANSFERRED => self::TRANSFERRED,
         self::CANCELLED => self::CANCELLED,
     ];
 }

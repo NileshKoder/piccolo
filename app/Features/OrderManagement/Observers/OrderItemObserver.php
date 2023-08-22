b6cf0e460090a6dfa774fc9d778668c3502d2000
NileshKoder <nileshsulakhe1@gmail.com>
2023-08-23 01:39:30 +0530

    change: remove auto complete order process

--- app/Features/OrderManagement/Observers/OrderItemObserver.php
+++ app/Features/OrderManagement/Observers/OrderItemObserver.php
@@ -9,18 +9,6 @@ class OrderItemObserver
 {
     public function updated(OrderItem  $orderItem)
     {
-        if($orderItem->isDirty('state')) {
-            if ($orderItem->state == OrderItem::TRANSFERRED) {
-                $this->processUpdateOrderState($orderItem->order);
-            }
-        }
-    }
-
-    public function processUpdateOrderState(Order $order)
-    {
-        if($order->orderItems()->count() == $order->orderItems()->where('state', OrderItem::TRANSFERRED)->count())
-        {
-            $order->updateState(Order::COMPLETED);
-        }
+        //
     }
 }

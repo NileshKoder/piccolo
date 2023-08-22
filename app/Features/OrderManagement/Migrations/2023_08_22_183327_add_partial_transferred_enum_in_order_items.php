9e3586972c798e705b9e3ad1930c4eb00fe87fce
NileshKoder <nileshsulakhe1@gmail.com>
2023-08-23 00:33:22 +0530

    chore: change add enums

---
+++ app/Features/OrderManagement/Migrations/2023_08_22_183327_add_partial_transferred_enum_in_order_items.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddPartialTransferredEnumInOrderItems extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('order_items', function (Blueprint $table) {
+            DB::statement("ALTER TABLE `order_items` CHANGE `state` `state` ENUM('CREATED','PARTIAL_MAPPED','MAPPED','PARTIAL_TRANSFERRED','TRANSFERED','CANCELLED') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'CREATED';");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('order_items', function (Blueprint $table) {
+            $table->enum('state', ["CREATED", "PARTIAL_MAPPED", "MAPPED", "TRANSFERED", "CANCELLED"])->default("CREATED");
+        });
+    }
+}

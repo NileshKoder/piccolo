ea3ca50f3c82628460fee47960a17c6d0eb4ebb7
NileshKoder <nileshsulakhe1@gmail.com>
2023-08-23 02:01:28 +0530

    change: order state to TRANSFERRED

--- app/Features/OrderManagement/Observers/OrderItemPalletObserver.php
+++ app/Features/OrderManagement/Observers/OrderItemPalletObserver.php
@@ -2,17 +2,19 @@
 
 namespace App\Features\OrderManagement\Observers;
 
+use App\Features\OrderManagement\Domains\Models\Order;
 use App\Features\OrderManagement\Domains\Models\OrderItem;
 use App\Features\OrderManagement\Domains\Models\OrderItemPallet;
 
 class OrderItemPalletObserver
 {
-    public function deleting(OrderItemPallet $orderItemPallet)
+    public function deleted(OrderItemPallet $orderItemPallet)
     {
-       if($orderItemPallet->orderItem->orderItemPalletDetails->whereNull('transfered_by')->count() == 0) {
+        if($orderItemPallet->orderItem->orderItemPallets->count() == 0) {
            $orderItemPallet->orderItem->updateState(OrderItem::TRANSFERRED);
-       } else {
+           $orderItemPallet->orderItem->order->updateState(Order::TRANSFERRED);
+        } else {
            $orderItemPallet->orderItem->updateState(OrderItem::PARTIAL_TRANSFERRED);
-       }
+        }
     }
 }

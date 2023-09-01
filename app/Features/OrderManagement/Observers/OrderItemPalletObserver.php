acaa22928de50876870c0fd639843c986114db0f
NileshKoder <nileshsulakhe1@gmail.com>
2023-09-01 10:56:20 +0530

    change: check is order item cancel or not

--- app/Features/OrderManagement/Observers/OrderItemPalletObserver.php
+++ app/Features/OrderManagement/Observers/OrderItemPalletObserver.php
@@ -10,13 +10,15 @@ class OrderItemPalletObserver
 {
     public function deleted(OrderItemPallet $orderItemPallet)
     {
-        if($orderItemPallet->orderItem->orderItemPallets->count() == 0) {
-           $orderItemPallet->orderItem->updateState(OrderItem::TRANSFERRED);
-           if ($orderItemPallet->orderItem->order->orderItems->where('state', OrderItem::TRANSFERRED)->count() == $orderItemPallet->orderItem->order->orderItems->count()) {
-               $orderItemPallet->orderItem->order->updateState(Order::TRANSFERRED);
-           }
-        } else {
-           $orderItemPallet->orderItem->updateState(OrderItem::PARTIAL_TRANSFERRED);
+        if($orderItemPallet->orderItem->state != OrderItem::CANCELLED) {
+            if ($orderItemPallet->orderItem->orderItemPallets->count() == 0) {
+                $orderItemPallet->orderItem->updateState(OrderItem::TRANSFERRED);
+                if ($orderItemPallet->orderItem->order->orderItems->where('state', OrderItem::TRANSFERRED)->count() == $orderItemPallet->orderItem->order->orderItems->count()) {
+                    $orderItemPallet->orderItem->order->updateState(Order::TRANSFERRED);
+                }
+            } else {
+                $orderItemPallet->orderItem->updateState(OrderItem::PARTIAL_TRANSFERRED);
+            }
         }
     }
 }

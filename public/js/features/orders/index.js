82310c52a18d00af6f05e9d1dba9e41a08dadb4a
NileshKoder <nileshsulakhe1@gmail.com>
2023-09-01 14:53:53 +0530

    change: add filter

--- public/js/features/orders/index.js
+++ public/js/features/orders/index.js
@@ -13,9 +13,16 @@ var initWareHouseDataTable = function(route) {
                 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
             },
             type: "POST",
-            data: function(d) { },
+            data: function(d) {
+                d.sku_code_id = $('#sku_code_id').val()
+                d.variant_id = $('#variant_id').val()
+                d.location_id = $('#location_id').val()
+                d.state = $('#state').val()
+                d.pickup_date = $('#pickup_date').val()
+            },
             error: function(e) {
                 console.log(e)
+                toastr.error('something went wrong');
             }
         },
         columns: [
@@ -34,14 +41,16 @@ var initWareHouseDataTable = function(route) {
         order: [[1, 'desc']],
     });
 
-    // $('#searchPalletCreation').on('click', function () {
-    //     table.draw();
-    // });
+    $('#search_order').on('click', function () {
+        table.draw();
+    });
 
-    // $('#clearPalletCreation').on('click', function () {
-    //     $('#pallet_id').val("").select2();
-    //     $('#crate_code_creation_id').val("").select2();
-    //     $('#location_id').val("").select2();
-    //     table.draw();
-    // });
+    $('#clear_order').on('click', function () {
+        $('#sku_code_id').val("").select2();
+        $('#variant_id').val("").select2();
+        $('#location_id').val("").select2();
+        $('#state').val("").select2();
+        $('#pickup_date').val("");
+        table.draw();
+    });
 };
